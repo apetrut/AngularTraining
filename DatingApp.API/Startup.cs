276c57929a9461ef059ad2f7e857c95882dec4eb
apetrut <apetrut@gmail.com>
2020-05-05 18:48:14 +0300

    bug fixes

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -54,16 +54,14 @@ namespace DatingApp.API
             {
                 app.UseExceptionHandler(builder => {
                     builder.Run(async context => {
-
                         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                         var error = context.Features.Get<IExceptionHandlerFeature>();
-
                         if (error != null)
                         {
                             context.Response.AddApplicationError(error.Error.Message);
                             await context.Response.WriteAsync(error.Error.Message);
                         }
-                    })
+                    });
                 });
             }
 
